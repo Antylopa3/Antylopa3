6c3588bbd078737d1e5e9e8c2eafb358fe76ab01
Antylopa3 <Makaron0-=>
2022-09-15 18:17:07 +0200

    15 września g.18.16

---
+++ MŻinstrukcje.py
@@ -0,0 +1,29 @@
+# import random
+#
+# lista = ["jeden", 7.7, "dwa", "trzy", [4, 5, 6], 8.8, 9.9]
+# print(type(lista))
+# print(lista[4][2])  # wycinek (lista wielowymiarowa)
+# print(lista[0:3])  # wycinek
+# print(lista[0], lista[5])
+# print(lista[-3:])  # wycinek od końca
+# lista.insert(2, "Artur")  # dodanie w dany indeks
+# lotek = {1, 3, 1, 4, 5, 9, 5}
+# print(type(lotek))
+# print(lotek)
+# print(5 in lotek)
+# import random
+# l = ['lola', "misia", "gruby"]
+# k6 = random.random() * 10
+# print(int(k6))
+# napis = "Krzysztof Kowalski"
+# print(napis.count("K"))
+# print(napis[0:5])
+# print(napis.capitalize())
+# mirek = [Mirek]
+# marek
+licznik = 0
+suma = 0
+while licznik < 100:
+      licznik += 1
+      suma = licznik + suma
+print(suma)
