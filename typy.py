3e900f12e431113f41e4500583febb46688f60f6
Żelasko Marian - Hurt <Marian.Zelasko@telekomunikacja.pl>
2022-09-15 13:43:34 +0200

    Initial commit

---
+++ typy.py
@@ -0,0 +1,20 @@
+# przykład kometarza z płotkiem
+# int: liczby całkowite int: 1, 2, -1, -2
+# float: liczby zmiennoprzecinkowe float: 5.5, 2.3
+# complex: liczby zespolone complex: 3+2j
+# bool: typ logiczny bool: True=1, False=0
+# lista: typ lista []
+print(5 / 2)
+print(5 // 2)  # dzielenie bez reszty ( zaokrąglenie )
+print(5 ** 2)  # potęgowanie
+print(5 * 2)
+# ctrl +D - powielanie
+# ctrl + alt + l - porządkowanie spacjami
+print('tekst' * 10)
+print('coś')
+print(["jeden", "dwa", 4, 5, 6, 7.9])
+imie = "Marian"
+print(type(imie))
+imie = int("5")
+print(type(imie))
+# zmienne piszemy z małych liter - tak się przyjęło, choć można inaczej. Jeżeli piszemy dużymi literami to oznacza żeby jej nie zmieniać
